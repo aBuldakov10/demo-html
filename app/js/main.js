48bbfcb597c9d869f3cc60ece8be6704dffea793
Buldakov <abuldakov10@gmail.com>
2022-11-15 14:55:10 +0400

    Add dark-light theme

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,94 @@
 document.addEventListener('DOMContentLoaded', () => {
+  /*** Init select list ***/
+  const select = $('.js-select2');
+
+  if (select) {
+    select.each(function () {
+      $(this).select2({
+        minimumResultsForSearch: 20,
+        dropdownParent: $(this).next('.dropDownSelect2'),
+        width: '100%',
+      });
+    });
+  }
+
+  /*** Fill preferences ***/
+  if (sessionStorage.getItem('preferences')) {
+    setPreferences();
+  }
+
+  /*** Change theme ***/
+  const form = document.querySelector('[name="preferences"]');
+
+  if (form) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const formData = new FormData(e.target);
+      let preferencesObj = {};
+
+      formData.forEach((value, fieldName) => {
+        preferencesObj[fieldName] = value;
+      });
+
+      sessionStorage.setItem('preferences', JSON.stringify(preferencesObj));
+
+      setPreferences();
+    });
+  }
+
+  /*** Preferences function ***/
+  function setPreferences() {
+    const getStoragePreferences = sessionStorage.getItem('preferences');
+    const getPreferencesObj = JSON.parse(getStoragePreferences);
+    const root = document.querySelector('html');
+    const textInput = document.querySelectorAll('[type="text"]');
+    const textRadio = document.querySelectorAll('[type="radio"]');
+
+    // Add theme class to body
+    root.setAttribute('data-theme', getPreferencesObj.theme);
+
+    // Fill text fields
+    textInput.forEach((inputElement) => {
+      inputElement.value = getPreferencesObj[inputElement.name];
+    });
+
+    // Set radio option
+    textRadio.forEach((radioElement) => {
+      if (
+        radioElement.getAttribute('name') === 'theme' &&
+        radioElement.value === getPreferencesObj.theme
+      ) {
+        radioElement.setAttribute('checked', true);
+      }
+    });
+
+    // Set selected option
+    Object.keys(getPreferencesObj).forEach((item) => {
+      const select = document.querySelector(`select[name="${item}"]`);
+
+      if (select) {
+        select.querySelectorAll('option').forEach((optionElement) => {
+          if (optionElement.value === getPreferencesObj[item]) {
+            optionElement.setAttribute('selected', true);
+
+            $('.js-select2').select2({ minimumResultsForSearch: 20 });
+          }
+        });
+      }
+    });
+
+    // Set container width
+    if (getPreferencesObj.containerWidth === '0') {
+      document.documentElement.style.setProperty('--container-width', '');
+    } else {
+      document.documentElement.style.setProperty(
+        '--container-width',
+        `${getPreferencesObj.containerWidth}px`
+      );
+    }
+  }
+
   /*** Mobile menu ***/
   const body = document.querySelector('body');
   const header = document.querySelector('header');
